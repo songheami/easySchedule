c397bb6e333023dbd217c8ec63beee027f7e35b1
송혜미 <workinghamee@gmail.com>
2021-06-19 23:17:51 +0900

    배포 설정 변경 (travis, s3, aws deploy group..)

---
+++ src/main/java/com/schedule/easy/springboot/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.schedule.easy.springboot.config;
+
+import com.schedule.easy.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
